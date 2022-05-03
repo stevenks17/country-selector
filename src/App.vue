3aeae70ccafa9c32c1b454c16343370d0362e92e
steven <stevenks17@gmail.com>
2022-05-03 12:17:47 -0400

    WIP selection dropdown

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,91 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    
+    <select class ="country-selector" @click="toggleDropdown">
+      <option v-for="country in countries" :key="country.id"> {{ country.name }} </option>
+    </select>
+
+    <select class ="state-selector" @click="toggleDropdown">
+      <option v-for=" state in countries.states" :key="state.id"> {{ country.state.name }} </option>
+    </select>
+
+
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+// let req = unirest("GET", "https://www.universal-tutorial.com/api/getaccesstoken");
 
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
+//   req.headers({
+//     "Accept": "application/json",
+//     "api-token": "zf-MNrWY3vB3QpuyRqBBJafoaNSgPEejxskw4m7EcNWDxnI976PDZaMhIe3srD_SgrY",
+//     "user-email": "abc@gmail.com"
+//   });
+const axios = require('axios').default;
+export default{
+  data() {
+    return {
+      active: false,
+      countries: [
+        {name: 'United States', 
+         id: 1,
+         states: [
+                  {name: 'New York', id: 1},
+                  { name: 'Boston', id: 2}
+                 ]
+        },
+        {name: 'United Kingdom', 
+         id: 2,
+         states: [
+                  {name: 'Belfast', id: 1},
+                  {name: 'England', id: 2}
+                 ]
+         },
+        {name: 'Europe', id: 3,
+         states: [
+                  {name: 'Germany', id: 1},
+                  {name: 'France', id: 2}
+                 ]
+        },
+      ],
+      selectedCountries: null,
+      selectedStates: null
+    }
+  },
+
+  mounted () {
+    axios
+      .get('https://www.universal-tutorial.com/api/countries/',{
+        headers: {
+        "Access-Control-Allow-Origin": "*",
+        "Accept": "application/json",
+        "api-token": "zf-MNrWY3vB3QpuyRqBBJafoaNSgPEejxskw4m7EcNWDxnI976PDZaMhIe3srD_SgrY",
+        "user-email": "stevenks17@gmail.com"
+        }
+      })
+
+      .then(function (response) {
+        console.log(response)
+      })
+
+      .catch(function (error) {
+         console.log(error)
+      })
+    },
+
+
+  methods: {
+    toggleDropdown() {
+      this.active = !this.active
+    },
+
+    }
   }
-}
+
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+.country-selector {
+
 }
 </style>
