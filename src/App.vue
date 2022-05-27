f57f3ef673cd397e567fd5f71dda16a19db96d42
steven <stevenks17@gmail.com>
2022-05-27 14:08:17 -0400

    WIP refactor

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,150 @@
 <template>
   <div id="app">
-    <dropdown />
+        <section v-if="errored">
+          <p class="error">We're sorry, we're not able to retrieve this information at the moment, please try back later</p>
+        </section>
+    <dropdown
+      title="Country State City Dropdown Demo"  
+      :selectedOption="selectedCountry"
+      :options="countries"
+      @fetch="fetchStates"
+      defaultValue="Country"
+    />
+
+    <dropdown  
+      :selectedOption="selectedState"
+      :options="states"
+      @fetch="fetchCities"
+      defaultValue="State"
+
+
+    />
+
+    <dropdown  
+      :selectedOption="selectedCity"
+      :options="cities"
+      defaultValue="City"
+      @fetch="selectedCity = $event.target.value"
+    />
+
+    <div>
+      Country: {{ selectedCountry }} 
+    </div>
+
+    
+    <div>
+      State: {{ selectedState }} 
+    </div>
+    
+    <div>
+      City: {{ selectedCity }} 
+    </div>
   </div>
+
+
 </template>
 
 <script>
 import dropdown from './components/dropdown.vue'
+
+const axios = require("axios").default;
+
 export default {
+
   components: { 
     dropdown
   },
-}
 
+  data(){
+    return {
+      countries: [],
+      cities: [],
+      states: [],
+      selectedCountry: "",
+      selectedState: "",
+      selectedCity: "",
+      city: "",
+      token:"",
+      errored: false,
+    }
+
+  },
+
+     created(){
+       this.getToken()
+            .then(()=> this.fetchCountries() )
+      
+    },
+  methods: {
+    getToken(){
+    return   axios.get("https://www.universal-tutorial.com/api/getaccesstoken/", {
+          headers: {
+              "Accept": "application/json",
+              "api-token":"zf-MNrWY3vB3QpuyRqBBJafoaNSgPEejxskw4m7EcNWDxnI976PDZaMhIe3srD_SgrY",
+              "user-email": "stevenks17@gmail.com"
+          }
+        }) 
+        .then(response => {
+          this.token = response.data.auth_token
+        })
+      },
+     fetchCountries() {
+       axios.get("https://www.universal-tutorial.com/api/countries/", {
+        headers: {
+            "Authorization": `Bearer ${this.token}`,
+            "Accept": "application/json"
+        }
+      }) 
+      .then(response => {
+        this.countries = response.data.map( country => ({value:country.country_name}))
+      })
+      .catch( error => {
+        console.log(error)
+        this.errored = true
+      })
+
+
+    },
+       fetchStates(event) {
+         this.selectedCountry = event.target.value
+
+        axios.get(`https://www.universal-tutorial.com/api/states/${event.target.value}`, {
+          headers: {
+              "Authorization": `Bearer ${this.token}`,
+              "Accept": "application/json"
+          }
+        }) 
+        .then(response => {
+          this.states = response.data.map( state => ({value:state.state_name}))
+
+        })
+        .catch( error => {
+          console.log(error)
+          this.errored = true
+        })
+        console.log(this.states)
+
+    },
+      fetchCities(event) {
+        this.selectedState = event.target.value
+        axios.get(`https://www.universal-tutorial.com/api/cities/${event.target.value}`, {
+          headers: {
+              "Authorization": `Bearer ${this.token}`,
+              "Accept": "application/json"
+          }
+        }) 
+        .then(response => {
+          this.cities = response.data.map( country => ({value:country.city_name}))
+        })
+        .catch( error => {
+          console.log(error)
+          this.errored = true
+        })
+
+        console.log(this.cities)
+    },
+  }
+}
 
 </script>
 
--- src/components/dropdown.vue
+++ src/components/dropdown.vue
@@ -1,153 +1,34 @@
 <template>
 
     <div class="dropdown-container" >
-      <h4 class="title">Country State City Dropdown Demo</h4>
-        <section v-if="errored">
-          <p class="error">We're sorry, we're not able to retrieve this information at the moment, please try back later</p>
-        </section>
+      <h4 class="title">{{ title }}</h4>
       <select class="selector" 
-              v-model="selectedCountry" 
-              @click="getCountries"
+              @change="$emit('fetch', $event)"
+ 
       >
-        <option value="">Select a Country</option>
-        <option v-for="(country, id) in countries" 
+        <option value="">Select a {{ defaultValue }}</option>
+        <option v-for="(option, id) in options" 
                 :key="id"
-                :value="country.country_name">
-          {{ country.country_name }}
+                :value="option.value"
+                >
+          {{ option.value }}
         </option>
       </select>
       <br>
-
-      <select class="selector" 
-              v-model="selectedState" 
-              @click="getStates"
-              :disabled="countries.length === 0"
-      >
-        <option value="">Select a State</option>
-        <option v-for="(state, id) in states" 
-                :key="id"
-                :value="state.state_name">
-          {{ state.state_name }}
-        </option>
-      </select>
-      <br>
-
-      <select class="selector" 
-              v-model="selectedCity" 
-              @click="getCities" 
-              :disabled="states.length === 0"
-      >
-        <option value="">Select a City</option>
-        <option v-for="(city, id) in cities" 
-                :key="id"
-                :value="city.city_name">
-          {{ city.city_name }}
-        </option>
-      </select>
-
-      <div class="selected">
-        "Country": {{ selectedCountry }} 
-        "State": {{ selectedState }} 
-        "City": {{ selectedCity }}
-      </div>
-
     </div>
 </template>
 
 <script>
 
-const axios = require("axios").default;
 
 export default {
-  data() {
-    return {
-        countries: [],
-        cities: [],
-        states: [],
-        selectedCountry: "",
-        selectedState: "",
-        selectedCity: "",
-        city: "",
-        token:"",
-        errored: false,
+  props:{
+    title: String,
+    options: Array,
+    selectedOption: String,
+    defaultValue: String
 
-      };
-    },
-
-     created(){
-       this.getToken()
-     },
-
-   methods: {
-
-     getToken(){
-       axios.get("https://www.universal-tutorial.com/api/getaccesstoken/", {
-          headers: {
-              "Accept": "application/json",
-              "api-token":"zf-MNrWY3vB3QpuyRqBBJafoaNSgPEejxskw4m7EcNWDxnI976PDZaMhIe3srD_SgrY",
-              "user-email": "stevenks17@gmail.com"
-          }
-        }) 
-        .then(response => {
-          this.token = response.data.auth_token
-        })
-      },
-
-
-     
-     async getCountries() {
-       await axios.get("https://www.universal-tutorial.com/api/countries/", {
-          headers: {
-              "Authorization": `Bearer ${this.token}`,
-              "Accept": "application/json"
-          }
-        }) 
-        .then(response => {
-          this.countries = response.data
-        })
-        .catch( error => {
-          console.log(error)
-          this.errored = true
-        })
-      console.log(this.countries)
-    },
-
-     async getStates() {
-       await axios.get(`https://www.universal-tutorial.com/api/states/${this.selectedCountry}`, {
-          headers: {
-              "Authorization": `Bearer ${this.token}`,
-              "Accept": "application/json"
-          }
-        }) 
-        .then(response => {
-          this.states = response.data
-        })
-        .catch( error => {
-          console.log(error)
-          this.errored = true
-        })
-        console.log(this.states)
-    },
-
-     async getCities() {
-       await axios.get(`https://www.universal-tutorial.com/api/cities/${this.selectedState}`, {
-          headers: {
-              "Authorization": `Bearer ${this.token}`,
-              "Accept": "application/json"
-          }
-        }) 
-        .then(response => {
-          this.cities = response.data
-        })
-        .catch( error => {
-          console.log(error)
-          this.errored = true
-        })
-
-        console.log(this.cities)
-    },
   },
-
 }
 
 
